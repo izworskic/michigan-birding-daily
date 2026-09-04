c296da69d4ea53679e7e0f83b8cd2329c019b094
Chris Izworski <46615230+izworskic@users.noreply.github.com>
2026-09-04 07:11:07 -0400

    Track Birding Daily profile page

---
+++ api/chris-izworski-page.js
@@ -0,0 +1,23 @@
+import profileHandler from './chris-izworski.js';
+
+const MEASUREMENT_ID = 'G-Y5D2V2W7HN';
+const GA4_TAG = `<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', '${MEASUREMENT_ID}');
+</script>`;
+
+export default async function handler(req, res) {
+  const originalSend = res.send.bind(res);
+  res.send = (body) => {
+    let output = body;
+    if (typeof output === 'string' && /<\/head>/i.test(output) && !output.includes(MEASUREMENT_ID)) {
+      output = output.replace(/<\/head>/i, `${GA4_TAG}\n</head>`);
+    }
+    return originalSend(output);
+  };
+  return profileHandler(req, res);
+}
